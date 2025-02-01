689548bb8813a7836cf17c0954f70af0b61c1086
reindert-vetter <reindertvetter@gmail.com>
2025-02-01 23:06:41 +0100

    Use match for routing

--- App/Http/Routes/Admin.php
+++ App/Http/Routes/Admin.php
@@ -6,11 +6,18 @@ namespace App\Http\Routes;
 
 use App\Http\Entity\View;
 
+/**
+ * Here you define your admin routes.
+ * Feel free to modify this class to suit your needs
+ *
+ * Other routes to match?
+ * Create a new class and register it in App/Bootstrap/Bootstrap.php
+ */
 class Admin
 {
     public static function canRender(): bool
     {
-        // Ensure /admin_frontend does not match
+        // Match /admin and /admin/*, but not /admin_frontend
         return request()->uri() === '/admin' || str_starts_with(request()->uri(), '/admin/');
     }
 
--- App/Http/Routes/Website.php
+++ App/Http/Routes/Website.php
@@ -6,6 +6,15 @@ namespace App\Http\Routes;
 
 use App\Http\Entity\View;
 
+/**
+ * Here you define your website routes.
+ * Feel free to modify this class to suit your needs.
+ * You can even replace the match statement with a Symfony router,
+ * or the standalone router of Laravel Illuminate Router.
+ *
+ * Other routes to match?
+ * Create a new class and register it in App/Bootstrap/Bootstrap.php
+ */
 class Website
 {
     public static function canRender(): bool
@@ -13,30 +22,19 @@ class Website
         return true;
     }
 
-    /** @noinspection PhpSwitchCanBeReplacedWithMatchExpressionInspection */
     public static function render(): View
     {
-        switch (true) {
-            case request()->uri() === '/':
-                return new View('website.homepage.homepage');
-            case request()->uri() === '/waiting-list':
-                return new View('website.login');
-            case request()->uri() === '/waiting-callback':
-                return new View('website.login_callback');
-            case request()->uri() === '/auth/callback':
-                return new View('website.includes.auth.callback');
-            case request()->uri() === '/pricing':
-                return new View('website.pricing.pricing');
-            case request()->uri() === '/contact':
-                return new View('website.contact.contact');
-            case request()->uri() === '/privacy-policy':
-                return new View('website.privacy-policy.privacy-policy');
-            case request()->uri() === '/blogs':
-                return new View('website.blog.overview');
-            case str_starts_with(request()->uri(), '/blogs/'):
-                return new View('website.blog.detail');
-            default:
-                return new View('website.404');
-        }
+        return match (true) {
+            request()->uri() === '/'                     => new View('website.homepage.homepage'),
+            request()->uri() === '/waiting-list'         => new View('website.login'),
+            request()->uri() === '/waiting-callback'     => new View('website.login_callback'),
+            request()->uri() === '/auth/callback'        => new View('website.includes.auth.callback'),
+            request()->uri() === '/pricing'              => new View('website.pricing.pricing'),
+            request()->uri() === '/contact'              => new View('website.contact.contact'),
+            request()->uri() === '/privacy-policy'       => new View('website.privacy-policy.privacy-policy'),
+            request()->uri() === '/blogs'                => new View('website.blog.overview'),
+            str_starts_with(request()->uri(), '/blogs/') => new View('website.blog.detail'),
+            default                                      => new View('website.404'),
+        };
     }
 }
