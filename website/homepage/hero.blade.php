fe6d74424614378a2d9e81c71f83faae99ebb4e4
reindert-vetter <reindertvetter@gmail.com>
2025-03-01 22:51:39 +0100

    Remove color component

--- website/homepage/hero.blade.php
+++ website/homepage/hero.blade.php
@@ -14,7 +14,9 @@
             </div>
             @endif
             <div class="text-center">
-                <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">{{ $hero->text('title')->default('Data to enrich your online business') }}</h1>
+                <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+                    {{ $hero->text('title')->default('Data to enrich your online business') }}
+                </h1>
                 <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">{{ $hero->text('subtitle')->max(200) }}</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
                     <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{ $hero->text('cta_text')->default('Get started') }}</a>
