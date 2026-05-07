be26047b9711f360faa1f1f9c5d42c7eeaf5613d
reindert-vetter <reindertvetter@gmail.com>
2026-05-07 22:22:19 +0200

    Use $model as a simple base

--- website/blog_detail.blade.php
+++ website/blog_detail.blade.php
@@ -1,22 +1,22 @@
 @php
     $alias = str_replace('/blogs/', '', request()->uri());
-    $blog = \model\blog_overview\blog_list::query()->whereSlugIs($alias)->first();
+    $model = \model\blog_overview\blog_list::query()->whereSlugIs($alias)->first();
 @endphp
-@section('head_title', $blog->title)
+@section('head_title', $model->title)
 @section('head_description', 'You can edit the meta description in blog_detail.blade.php')
 @extends('website.layouts.main')
 
 @section('content')
-<main class="js-edit:{{ $blog->getId() }} max-w-3xl mx-auto">
+<main class="js-edit:{{ $model->getId() }} max-w-3xl mx-auto">
     <article class="relative p-12">
         <a href="/blogs" class="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2">Back to overview</a>
         <div class="rounded-lg p-4 text-2xl flex justify-center m-8">
-            <h1>{{ $blog->title }}</h1>
+            <h1>{{ $model->title }}</h1>
         </div>
         <div class="font-body">
             <div class="mx-4 w-full">
-                {!! $blog->image('image')->widthPx(800)->getPicture(class: 'relative w-full sm:w-220 p-3 rounded-lg') !!}
-                @include('pkg.confetti-cms.content.blocks.index', ['model' => $blog->content('content')])
+                {!! $model->image('image')->widthPx(800)->getPicture(class: 'relative w-full sm:w-220 p-3 rounded-lg') !!}
+                @include('pkg.confetti-cms.content.blocks.index', ['model' => $model->content('content')])
             </div>
         </div>
         <div class=" flex items-center justify-center">
